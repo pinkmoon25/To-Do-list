bea58fe0be18b03328128681043cc7cf6c0d54e0
Nicholas Emmanuel Chimaobi <nicholasemmanuel321@gmail.com>
2022-07-13 14:05:56 +0100

    add parameter to the input

--- src/task.test.js
+++ src/task.test.js
@@ -2,11 +2,11 @@
  * @jest-environment jsdom
  */
 
-const {addTask, removeTask} = require('./task');
-let taskArr = require('./task');
+const { addTask, removeTask } = require('./task.js');
+let taskArr = require('./task.js');
 
-describe('addTask', ()=>{
-  test('add item', ()=>{
+describe('addTask', () => {
+  test('add item', () => {
     let body = document.querySelector('body');
     let listInput = document.createElement('input');
     listInput.id = 'listInput';
@@ -15,14 +15,13 @@ describe('addTask', ()=>{
     listInput.value = 'hello';
     addTask(listInput, taskArr);
     expect(taskArr).toHaveLength(1);
-  })
+  });
 });
 
-describe('removeTask', ()=>{
-  test('remove item', ()=>{
+describe('removeTask', () => {
+  test('remove item', () => {
     const id = 0;
     removeTask(id, taskArr);
     expect(taskArr).toHaveLength(0);
-  })
+  });
 });
-
